16e77e22b0cda6b9e739c540cf653e98f069b9e9
basil lizotte <basil@btcl-advisory.com>
2021-05-16 06:09:40 +0200

    blank dashboard controller & review or routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :shoes do
-    resources :locations, only [:create] 
+    resources :locations, only: [:create]
   end  
-  resources :locations [:update, :edit, :destroy]
-  resources :dashboard, only [:index] 
+  resources :locations, only: [:update, :edit, :destroy]
+  resources :dashboard, only: [:index] 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
