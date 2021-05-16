d6f89ad738f149cf92d7d20bf3d718fa00a9459d
basil lizotte <basil@btcl-advisory.com>
2021-05-16 05:12:34 +0200

    Shoe model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_130016) do
+ActiveRecord::Schema.define(version: 2021_05_17_130017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "shoes", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "brand"
+    t.string "model"
+    t.date "purchasing_date"
+    t.string "color"
+    t.integer "daily_pricing"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_shoes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_05_17_130016) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "shoes", "users"
 end
