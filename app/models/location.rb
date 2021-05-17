e2d4c7589b5dbf3cbc4168c531056bb2670366ef
Benjamin Eycken <be.eycken@gmail.com>
2021-05-17 16:52:06 +0200

    modif validation location

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,5 +4,4 @@ class Location < ApplicationRecord
 
   validates :date_beginning, presence: true
   validates :date_end, presence: true
-  validates :location_pricing, presence: true, format: { with: /\A\d+(?:\.\d{0,1})?\z/ }, numericality: { greater_than: 0, less_than: 1000 }
 end
