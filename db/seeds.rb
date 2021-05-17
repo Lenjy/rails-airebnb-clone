eb6fc563cf573515b44b4ad4c60911939f1d0372
Benjamin Eycken <be.eycken@gmail.com>
2021-05-17 17:14:13 +0200

    debug seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require_relative '../models/category'
+require_relative '../app/models/category'
 
 Category.create(name: "Sport")
 Category.create(name: "Sneakers")
