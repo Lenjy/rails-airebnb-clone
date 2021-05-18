93cc7f45f21c0d6e53706f2dd6d5383863ad62ee
Benjamin Eycken <be.eycken@gmail.com>
2021-05-18 17:06:14 +0200

    Big changes, it WORKS

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
 #   skip_before_action :authenticate_user! # Temporaire: Pour tester les vues
   skip_after_action :verify_policy_scoped 
   def index
+    @shoe = Shoe.new
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   validates :name, presence: true
+  has_many :shoes, through: :link_to_cats
 end
