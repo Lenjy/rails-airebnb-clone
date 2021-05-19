e10e141c41e8accbef04c3e796f7936f92159aa6
BrazoProgra <80844642+BadzoProgra@users.noreply.github.com>
2021-05-19 11:21:51 +0200

    Update location.rb

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,6 @@
 class Location < ApplicationRecord
   belongs_to :shoe
   belongs_to :user
-  has_one_attached :photo
 
   validates :date_beginning, presence: true
   validates :date_end, presence: true
